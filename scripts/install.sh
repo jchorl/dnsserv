9cf4b53f2656dcfaf5756e542692efe641f4a2ea
Josh Chorlton <jchorlton@gmail.com>
2018-11-19 20:09:25 -0800

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,1 @@
-sudo cp /home/j/dnsserv/scripts/run-pi.sh /etc/cron.hourly/
+sudo cp /home/j/dnsserv/scripts/run-pi.sh /etc/cron.hourly/run-pi # name cannot have . in it https://askubuntu.com/a/39732
